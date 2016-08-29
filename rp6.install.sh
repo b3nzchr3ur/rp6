4f7b69ee8eef32b1816081cc547a5d486da66d67
Kevin Dullens <kevindullens3@hotmail.com>
2016-08-29 18:01:40 +0200

    modified:   RobotLoader_20120924/robotflasher.sh 	modified:   rp6.install.sh

--- rp6.install.sh
+++ rp6.install.sh
@@ -18,11 +18,24 @@ function reportStep {
 set -e
 
 reportStep "Installing software needed for RP6:"
-sudo apt-get update
-sudo apt-get install gcc-avr avr-libc git wget openjdk-6-jdk
+#sudo apt-get update
+#sudo apt-get upgrade
+sudo apt-get install gcc-avr avr-libc git wget openjdk-8-jdk
 
 export GIT_CODE_URL="https://github.com/b3nzchr3ur/rp6.git"
 reportStep "Retrieving source code for RP6:" $GIT_CODE_URL
-git clone $GIT_CODE_URL
+git clone $GIT_CODE_URL $HOME/rp6
 
-reportStep "Installation done... enjoy!" 
+#makes sure you can access the robotflash.sh from anywhere
+echo "export PATH=$PATH:$HOME/rp6/RobotLoader_20120924" >> ~/.bashrc
+
+#Enables user to type "robotload" to start it anywhere
+if [[ $arch == "x86_64" ]] ; then 
+    echo "alias robotloader='temp_dir=\$(pwd) && cd $HOME/rp6/RobotLoader_20120924 && sudo ./robotloader_linux_x64.sh && cd \"\$temp_dir\"'" >> ~/.bashrc
+else
+    echo "alias robotloader='temp_dir=\$(pwd) && cd $HOME/rp6/RobotLoader_20120924 && sudo ./robotloader_linux.sh ; cd \"\$temp_dir\"'" >> ~/.bashrc
+fi
+
+source ~/.bashrc
+
+reportStep "Installation done... enjoy!"
