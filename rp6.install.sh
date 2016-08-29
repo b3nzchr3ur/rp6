b91b252c2bf10fbc1b2215dd8b9ff17bfd9efc4a
Kevin Dullens <kevindullens3@hotmail.com>
2016-08-29 18:12:08 +0200

    modified:   rp6.install.sh

--- rp6.install.sh
+++ rp6.install.sh
@@ -18,9 +18,9 @@ function reportStep {
 set -e
 
 reportStep "Installing software needed for RP6:"
-#sudo apt-get update
+sudo apt-get update
 #sudo apt-get upgrade
-sudo apt-get install gcc-avr avr-libc git wget openjdk-8-jdk
+sudo apt-get install gcc-avr avr-libc git wget openjdk-7-jdk
 
 export GIT_CODE_URL="https://github.com/b3nzchr3ur/rp6.git"
 reportStep "Retrieving source code for RP6:" $GIT_CODE_URL
